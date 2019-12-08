288d7ce96cd58a51a00354fe097bdd10d2f3b8e4
songlianfu_6000 <songlianfu@126.com>
2019-12-08 11:26:04 +0800

    分支测试提交

--- src/main/resources/db/migration/V5__replace_Id_Integer_to_Long_to_user_table.sql
+++ src/main/resources/db/migration/V5__replace_Id_Integer_to_Long_to_user_table.sql
@@ -1,2 +1,3 @@
-alter table USER
-    alter column ID LONG(10) default (NEXT VALUE FOR PUBLIC.SYSTEM_SEQUENCE_2F242FE2_F51C_4BF7_8F63_B950EC3EDF0B) auto_increment;
+/*alter table USER
+    alter column ID LONG(10) default auto_increment;*/
+alter table user alter column ID BIGINT default not null auto_increment;
--- src/main/resources/db/migration/V9__Change_id_column_type.sql
+++ src/main/resources/db/migration/V9__Change_id_column_type.sql
@@ -1,3 +1,3 @@
 alter table QUESTION alter column ID BIGINT default not null auto_increment;
-alter table USER alter column ID BIGINT default not null auto_increment;
+alter table user alter column ID BIGINT default not null auto_increment;
 
